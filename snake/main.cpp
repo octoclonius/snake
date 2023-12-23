61a8586b443e5c45de67c8e1c5b17c3b3da168b5
jporubci <jporubci@nd.edu>
2023-12-23 14:52:52 -0600

    Initial Commit

---
+++ snake/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  snake
+//
+//  Created by Jozef Porubcin on 12/23/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
